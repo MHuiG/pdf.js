5e551eb7b050814c56e63c5cc25157a698bb7fc2
MH <616001163@qq.com>
2020-02-09 16:31:47 +0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -63,7 +63,7 @@ var DIST_DIR = BUILD_DIR + "dist/";
 var COMMON_WEB_FILES = ["web/images/*.{png,svg,gif,cur}", "web/debugger.js"];
 var MOZCENTRAL_DIFF_FILE = "mozcentral.diff";
 
-var REPO = "git@github.com:mozilla/pdf.js.git";
+var REPO = "git@github.com:MHuiG/pdf.js.git";
 var DIST_REPO_URL = "https://github.com/mozilla/pdfjs-dist";
 
 var builder = require("./external/builder/builder.js");
